094c46e7c91fcadb16b110e138307d9dfb7b3ed0
Junior Santos <jr0796@gmail.com>
2023-01-20 23:37:04 -0300

    Criação da interface 01 - Entry's

--- cadastros/funcionario.py
+++ cadastros/funcionario.py
@@ -14,85 +14,104 @@ txtNome.place(width=440, height=25, x=15, y=38)
 
 #RG
 txtRg = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtRg.place(width=205, height=25, x=15, y=78)
+txtRg.place(width=205, height=25, x=15, y=88)
 
 #CPF
 txtCpf = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtCpf.place(width=205, height=25, x=250, y=78)
+txtCpf.place(width=205, height=25, x=250, y=88)
 
 #CEP
 txtCep = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtCep.place(width=205, height=25, x=15, y=138)
+txtCep.place(width=205, height=25, x=15, y=158)
 
 #estado
 txtEstado = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtEstado.place(width=205, height=25, x=250, y=138)
+txtEstado.place(width=205, height=25, x=250, y=158)
 
 #rua
 txtRua = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtRua.place(width=370, height=25, x=15, y=198)
+txtRua.place(width=370, height=25, x=15, y=218)
 
 #Número da rua
 txtnumero = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtnumero.place(width=60, height=25, x=395, y=198)
+txtnumero.place(width=60, height=25, x=395, y=218)
 
 
 #Bairro
 txtBairro = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtBairro.place(width=205, height=25, x=15, y=258)
+txtBairro.place(width=205, height=25, x=15, y=278)
 
 #Cidade
 txtCidade = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtCidade.place(width=205, height=25, x=250, y=258)
+txtCidade.place(width=205, height=25, x=250, y=278)
 
 #Complemento
 txtComplemento = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtComplemento.place(width=205, height=25, x=15, y=318)
+txtComplemento.place(width=205, height=25, x=15, y=338)
 
 
 #Telefone
 txtTelefone = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtTelefone.place(width=200, height=25, x=15, y=378)
+txtTelefone.place(width=205, height=25, x=15, y=398)
 #Caixa dos Telefones
 txtCaixaTelefones = Entry(master, bd=2, font=("Calibri, 12"), justify=CENTER)
-txtCaixaTelefones.place(width=205, height=75, x=15, y=418)
+txtCaixaTelefones.place(width=205, height=75, x=15, y=438)
 
 #Foto
 txtFoto = Entry(master, bd=1, font=("Calibri, 12"), justify=CENTER)
-txtFoto.place(width=110, height=115, x=350, y=348)
+txtFoto.place(width=110, height=115, x=345, y=372)
 #botão Add Foto
 btnAddFoto = Button(master, text='Add Foto',font=("Calibri, 12"), command="")
 btnAddFoto.pack(side ='top')
-btnAddFoto.place(width=110, height=25, x=350, y=466)
+btnAddFoto.place(width=110, height=25, x=345, y=490)
 
 
 #botão Add Fone
 btnAddFone = Button(master, text='+',font=("Calibri, 15"), command="")
 btnAddFone.pack(side ='top')
-btnAddFone.place(width=35, height=35, x=230, y=420)
+btnAddFone.place(width=35, height=35, x=230, y=440)
 #botão RM Fone
 btnRmFone = Button(master, text='-',font=("Calibri, 15"), command="")
 btnRmFone.pack(side ='top')
-btnRmFone.place(width=35, height=35, x=230, y=460)
+btnRmFone.place(width=35, height=35, x=230, y=480)
 
 
 #botão Confirmar
 btnConfirmar = Button(master, text='Confirmar',font=("Calibri, 12"), command=" ")
 btnConfirmar.pack(side ='top')
-btnConfirmar.place(width=160, height=45, x=300, y=525)
+btnConfirmar.place(width=160, height=45, x=300, y=545)
 
 #botão Cancelar
 btnCancelar = Button(master, text='Cancelar', font=("Calibri, 12"), command="master")
 btnCancelar.pack(side ='top')
-btnCancelar.place(width=160, height=45, x=130, y=525)
+btnCancelar.place(width=160, height=45, x=130, y=545)
 
 
+#Label Endereco
+lblTelefone = Label(master,text= "Nome: ",font = "Calibri, 11",).place(x=15, y=15)
 
-
-
-
-
-
+#Label RG
+lblRg = Label(master,text= "RG: ",font = "Calibri, 11",).place(x=15, y=65)
+#Label CPF
+lblCpf = Label(master,text= "CPF: ",font = "Calibri, 11",).place(x=250, y=65)
+#Label Cep
+lblCep = Label(master,text= "CEP: ",font = "Calibri, 11",).place(x=15, y=135)
+#Label Estado
+lblEstado = Label(master,text= "Estado: ",font = "Calibri, 11",).place(x=250, y=135)
+#Label Rua
+lblRua = Label(master,text= "Logradouro: ",font = "Calibri, 11",).place(x=15, y=195)
+#Label Número
+lblNum = Label(master,text= "Nº: ",font = "Calibri, 11",).place(x=395, y=195)
+#Label Bairro
+lblBairro = Label(master,text= "Bairro: ",font = "Calibri, 11",).place(x=15, y=255)
+#Label Cidade
+lblCidade = Label(master,text= "Cidade: ",font = "Calibri, 11",).place(x=250, y=255)
+#Label Complemento
+lblComplemento = Label(master,text= "Complemento: ",font = "Calibri, 11",).place(x=15, y=315)
+#Label Telefone
+lblTelefone = Label(master,text= "Telefone: ",font = "Calibri, 11",).place(x=15, y=375)
+#Label Foto
+lblFoto = Label(master,text= "Foto: ",font = "Calibri, 11",).place(x=345, y=349)
 
 master.mainloop()
 
