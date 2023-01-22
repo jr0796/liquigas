1166963d711275ad95c4f01e4e52880e82993084
Junior Santos <jr0796@gmail.com>
2023-01-21 23:22:04 -0300

    Criação da interface 03 - Tela Fornecedores

---
+++ cadastros/fornecedor.py
@@ -0,0 +1,16 @@
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+
+
+#Construção da Janela
+master = Tk()
+master.title("..:: Cadastro de Fornecedores::..")
+#master.iconbitmap(default=" ")
+master.geometry("480x600+400+50") #Largura x Altura + dist. Esquerda + dist. do topo
+
+
+
+
+
+master.mainloop()
