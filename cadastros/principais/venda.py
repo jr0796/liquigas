b015b0c4cffb4396f28a9c338aa3663f55959c16
Junior Santos <jr0796@gmail.com>
2023-01-30 15:22:21 -0300

    Criação da interface 05 - Tela de VEnda - Essa tela deu um trabalho norme .

--- cadastros/principais/venda.py
+++ cadastros/principais/venda.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import ttk
 
 
+
 from PIL import ImageTk, Image
 from tkinter.filedialog import askopenfilename
 
@@ -90,13 +91,92 @@ tv.place(x=440,y=22)
 #Dados Produto
 produtos = LabelFrame(master, text="Informações dos Produtos ", width= 1080, height=120)
 produtos.pack(expand=1)
+#Nome Produto
+lblNomeProduto = Label(produtos,text= "Nome do Produto: ",font = "Calibri, 11",).place(x=1, y=1)
+#Combo Produtos
+campoSelect = TK.StringVar()
+comboProdutos = ttk.Combobox(produtos, width=62, textvariable=(campoSelect))
+comboProdutos.place(x=1, y=20)
+comboProdutos['values'] = " "
+
+#botão Add Produto à itens da venda
+btnaddProdutoVenda = Button(produtos, text='+',font=("Calibri, 11"), command="")
+btnaddProdutoVenda.pack(side ='top')
+btnaddProdutoVenda.place(width=45, height=35, x=410, y=12)
+#botão rm Produto à itens da venda
+btnrmProdutoVenda = Button(produtos, text='-',font=("Calibri, 11"), command="")
+btnrmProdutoVenda.pack(side ='top')
+btnrmProdutoVenda.place(width=45, height=35, x=410, y=52)
+
+#TreeVew Itens da Lista
+tv = ttk.Treeview(produtos, selectmode="browse",column=("Produto","Valor","Qtde.","Sub-Total"),
+                  show='headings',height=11)
+tv.column("Produto", width=50, minwidth=0, stretch=NO)
+tv.heading("#1", text='Produto')
+tv.column("Valor", minwidth=0,width=250,stretch=NO)
+tv.heading("#2", text='Valor')
+tv.column("Qtde.", minwidth=0,width=100,stretch=NO)
+tv.heading("#3", text='Qtde.')
+tv.column("Sub-Total", minwidth=0,width=100,stretch=NO)
+tv.heading("#4", text='Sub-Total')
+tv.place(x=460,y=22)
+
+#Nome Valor do Produto
+lblValorProduto = Label(produtos,text= "Valor do Produto: ",font = "Calibri, 11",).place(x=1, y=42)
+lblPrecoProduto = Label(produtos,text= "R$: 10,00 ",font = "Calibri, 22",).place(x=1, y=60)
+
+#Nome Valor do Produto
+lblQtdeProduto = Label(produtos,text= "Quantidade: ",font = "Calibri, 11",).place(x=200, y=42)
+#Spinbox Quantidade Produto
+valorSelecionado = TK.StringVar(value=1)
+spinBox = ttk.Spinbox(produtos, from_=1,to=20,width=5, textvariable= valorSelecionado, wrap= True).place(x=200, y=65)
+
+#Nome Sub-total Item
+lblsubTotalItem = Label(produtos,text= "Sub-total Item: ",font = "Calibri, 11",).place(x=290, y=42)
+lblsubTotalItem = Label(produtos,text= "R$: 30,00 ",font = "Calibri, 22",).place(x=290, y=65)
+
 
 #Valores
-valores = LabelFrame(master, text="Valores da Venda", width= 1080, height=120)
-valores.pack(expand="no", anchor='n')
+valores = LabelFrame(master, text="Valores da Venda", width= 500, height=120)
+valores.pack(expand="no", anchor='w')
+
+#Label Total
+lblTotal = Label(valores,text= "Total: ",font = "Calibri, 11",).place(x=1, y=1)
+lblTotalResult = Label(valores,text= "R$: 200,00",font = "Calibri, 15",).place(x=1, y=20)
+
+#Label Valor REcebido
+lblRecebido = Label(valores,text= "Valor Recebido: ",font = "Calibri, 11",).place(x=200, y=1)
+#Valor Recebido
+txtRecebido= Entry(valores, bd=2, font=("Calibri, 12"), justify=LEFT)
+txtRecebido.place(width=120, height=25, x=200, y=20)
+#lbl Troco
+lblTroco = Label(valores,text= "Troco: ",font = "Calibri, 11",).place(x=400, y=1)
+lblResulTroco= Label(valores,text= "R$ 100,00 ",font = "Calibri, 15",).place(x=400, y=20)
+#Combo Forma de pagamento
+campoSelect = TK.StringVar()
+comboFormaPgto = ttk.Combobox(valores, width=62, textvariable=(campoSelect))
+comboFormaPgto.place(x=1, y=40)
+comboFormaPgto['values'] = " "
+#Radio Entraga
+#radioselected = Tk.StringVar()
+radio1 = ttk.Radiobutton(valores,text="Com entrega", value="Value1")
+radio1.place(x=200, y=40)
+radio2 = ttk.Radiobutton(valores,text="Sem entrega", value="Value2")
+radio2.place(x=300, y=40)
 
 
-
+#botão Confirmar
+btnConfirmar = Button(master, text='Confirmar',font=("Calibri, 12"), command=" ")
+btnConfirmar.pack(side ='top')
+btnConfirmar.place(width=160, height=45, x=900, y=565)
+#botão Cancelar
+btnCancelar = Button(master, text='Cancelar', font=("Calibri, 12"), command="cancelarSair")
+btnCancelar.pack(side ='top')
+btnCancelar.place(width=160, height=45, x=750, y=565)
+#botão Limpar
+btnLimpar = Button(master, text='Cancelar', font=("Calibri, 12"), command="cancelarSair")
+btnLimpar.pack(side ='top')
+btnLimpar.place(width=160, height=45, x=620, y=565)
 
 
 master.mainloop()
